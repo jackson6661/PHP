ef940f5d9e1263e2cd1532cfdce731eedb74b48a
Evgenii Podchasov <99173759+jackson6661@users.noreply.github.com>
2024-12-02 20:31:22 +0700

    Update text.php

--- text.php
+++ text.php
@@ -7,14 +7,15 @@
     <title>Web-page</title>
 </head>
 
-<body>
     <?php
-        echo 'Name - ' . $_FILES['filename']['name'] . '<br>';
-        echo 'Size - ' . $_FILES['filename']['size'] . '<br>';
-        echo 'Type - ' . $_FILES['filename']['type'] . '<br>';
-        echo 'Temporary file - ' . $_FILES['filename']['tmp_name'] . '<br>';
-
+            if ($_FILES['filename'][['size'] > 3*1024*1024]) {
+                exit("Sorry, your file is too large.");
+            }
             if(move_uploaded_file($_FILES['filename']['tmp_name'], 'temp\\'.$_FILES['filename']['name'])) {
+                echo 'Name - ' . $_FILES['filename']['name'] . '<br>';
+                echo 'Size - ' . $_FILES['filename']['size'] . '<br>';
+                echo 'Type - ' . $_FILES['filename']['type'] . '<br>';
+                echo 'Temporary file - ' . $_FILES['filename']['tmp_name'] . '<br>';
                 echo 'File successfully uploaded!';
             } else {
                 echo 'There was an error uploading your file!';
